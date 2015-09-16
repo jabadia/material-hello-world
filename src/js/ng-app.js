8a90924b219b2ca328dab31a2302523189c19d09
Javier Abadia <javi.abadia@gmail.com>
2015-09-16 16:41:34 +0200

    theming

--- src/js/ng-app.js
+++ src/js/ng-app.js
@@ -3,6 +3,24 @@
 
     var app = angular.module('HelloWorldApp',['ngMaterial']);
 
+    app.config(function($mdThemingProvider)
+    {
+        var customBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+            'contrastDefaultColor':'light',
+            'contrastDarkColors': ['50'],
+            '50': 'ffffff'
+        });
+        $mdThemingProvider.definePalette('customBlue', customBlueMap);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('customBlue', {
+                'default':'500',
+                'hue-1': '50'
+            })
+            .accentPalette('pink');
+        $mdThemingProvider.theme('input','default')
+            .primaryPalette('grey');
+    });
+
     app.controller('MainCtrl', function($scope, $mdSidenav)
     {
         $scope.toggleSidenav = function(menuId)
